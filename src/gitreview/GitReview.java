b4b88b99f17dea22a14cd1610a47c180e52d7f90
Gautmilk <gauthiky@sheridancollege.ca>
2020-01-23 14:18:23 -0500

    added address to student and modified studenlist

---
+++ src/gitreview/GitReview.java
@@ -0,0 +1,17 @@
+
+
+package gitreview;
+/**
+ * 
+ * @author theky
+ */
+
+public class GitReview {
+
+    public static void main(String[] args) {
+        
+        System.out.println("A simple example");
+       
+    }
+
+}
