e937e492783836167232e85bc923e71652d494a9
Gautmilk <59976546+Gautmilk@users.noreply.github.com>
2020-01-23 15:02:44 -0500

    Update GitReview.java
    from GitHub

--- src/gitreview/GitReview.java
+++ src/gitreview/GitReview.java
@@ -11,6 +11,8 @@ public class GitReview {
     public static void main(String[] args) {
         
         System.out.println("A simple example");
+        
+        System.out.println("Modified on GitHub");
        
     }
 
